5d91e36abacb9a4229f5ae4e1a10eda86fb9c40e
Keshav Jha <keshavjha0610@gmail.com>
2021-05-09 18:15:17 +0530

    Add files via upload

---
+++ HashsetDemo.java
@@ -0,0 +1,37 @@
+package ArrayListConcepts;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashsetDemo {
+
+	public static void main(String[] args) {
+		//Hashset,treeset,linkedhashset implements set interface
+		//does not accept duplicated values
+		//There is no guarantee that elements are stored in sequential order.random order it is stored
+		
+		HashSet<String> hs= new HashSet<String>();
+		hs.add("India");
+		hs.add("uk");
+		hs.add("Usa");
+		hs.add("Usa");
+		hs.add("he");
+		hs.add("she");
+		System.out.println(hs);
+		//System.out.println(hs.remove("uk"));
+		System.out.println(hs.isEmpty());
+		System.out.println(hs.size());
+		
+		//Iterator
+		Iterator<String> i=hs.iterator();
+		while(i.hasNext())
+		{
+		System.out.println(i.next());
+		}
+	   // System.out.println(i.next());//i.next goes down to the next index from top
+
+
+
+	}
+
+}
