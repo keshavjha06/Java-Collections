2e05f97b3b4ac9a39b277a41c4df1164618b62bf
Keshav Jha <keshavjha0610@gmail.com>
2021-06-24 18:57:27 +0530

    Add files via upload

---
+++ HashMapConcepts/HashMapInitialization.java
@@ -0,0 +1,27 @@
+package HashMapConcepts;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+public class HashMapInitialization {
+
+	public static void main(String[] args) {
+		HashMap<String, String> capitalMap = new HashMap<String,String>();
+		
+	//1.
+		
+		String str = "Testing";
+		System.out.println(str.lastIndexOf(""));
+		System.out.println(str.toCharArray().length);
+		
+		
+		
+		
+		
+		
+		}
+		
+	}
+
+
